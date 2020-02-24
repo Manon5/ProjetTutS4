5e18c93003581e7f9602143187baaa0dd559ec7d
Yonah125 <yonah@sfr.fr>
2020-02-24 18:29:23 +0100

    Marche que sur navi

---
+++ Projet/www/precache-manifest.1f790330aa30071ced3922b5578a3938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718ee40e636bb35131c9c727c0ce97ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a42076ffca66465815a2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "45531a1450832caf2f69",
+    "url": "/static/js/2.77f1a4c9.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/static/js/2.77f1a4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a42076ffca66465815a2",
+    "url": "/static/js/main.955e35ac.chunk.js"
+  },
+  {
+    "revision": "dce50d46630d821fa694",
+    "url": "/static/js/runtime-main.f96e5c4d.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
+import Geolocation from '@react-native-community/geolocation'
+
+var position = [49.119950,6.163673]
+
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+  console.log('Votre position actuelle est :');
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude : ${crd.longitude}`);
+  console.log(`La précision est de ${crd.accuracy} mètres.`);
+  position=[crd.latitude,crd.longitude];
+
+}
+
+function error(err) {
+  console.warn(`ERREUR (${err.code}): ${err.message}`);
+}
+
+type State = {
+  lat: number,
+  lng: number,
+  zoom: number,
+}
+
+export const pointerIcon = new L.Icon({
+  iconUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Bruce_Lee_1973.jpg/290px-Bruce_Lee_1973.jpg',
+  iconRetinaUrl: '../assets/pointerIcon.svg',
+  iconAnchor: [5, 55],
+  popupAnchor: [10, -44],
+  iconSize: [25, 55],
+  shadowUrl: '../assets/marker-shadow.png',
+  shadowSize: [68, 95],
+  shadowAnchor: [20, 92],
+})
+
+export const suitcasePoint = new L.Icon({
+  iconUrl: 'https://images-na.ssl-images-amazon.com/images/I/61Vh5DJkCKL._AC_SL1000_.jpg',
+  iconRetinaUrl: '../assets/suitcaseIcon.svg',
+  iconAnchor: [20, 40],
+  popupAnchor: [0, -35],
+  iconSize: [40, 40],
+  shadowUrl: '../assets/marker-shadow.png',
+  shadowSize: [29, 40],
+  shadowAnchor: [7, 40],
+})
+
+export default class CustomIcons extends Component {
+  constructor(props) {
+    super();
+
+    this.state={
+      marker:position,
+      draggable: true,
+
+
+    };
+
+
+  }
+
+
+  componentDidMount() {
+    Geolocation.getCurrentPosition(success, error, options);
+    this.setState({marker:position});
+   }
+
+   update = () =>{
+    Geolocation.getCurrentPosition(success, error, options);
+    this.setState({marker:position});
+
+
+  }
+
+  render() {
+
+
+    console.log(`La précision est de ${position} mètres.`);
+    const marker =
+      <Marker position={this.state.marker}
+
+            onLoad={setInterval(this.update, 2000)}>
+        <Popup>You are here</Popup>
+      </Marker>
+
+
+
+    return (
+      <Map center={position} zoom={17}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {marker}
+      </Map>
+    )
+  }
+}
