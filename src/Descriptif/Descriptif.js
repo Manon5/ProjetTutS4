5b9a1e7d23c3c3fe6e522ff0efb5a6212933df69
Yonah125 <yonah@sfr.fr>
2020-02-28 09:35:34 +0100

    Descri

--- src/Descriptif/Descriptif.js
+++ src/Descriptif/Descriptif.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
 import Button from "react-bootstrap/Button";
-
+import {Link} from 'react-router-dom';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
@@ -53,13 +53,58 @@ export default class Incontournables extends React.Component {
                 this.setState({ persons });
             })
     }
-    generate_url = (id) => {
-        return "https://devweb.iutmetz.univ-lorraine.fr/~ramier2u/monumix/images/monument" + id + "/image1.jpg"
-    }
-
 
 getUrl = (id) => {
-  return ("https://devweb.iutmetz.univ-lorraine.fr/~ramier2u/monumix/images/monument" + idee_monument.id + "/image" + id+ ".jpg");
+  return ("https://devweb.iutmetz.univ-lorraine.fr/~ramier2u/monumix/images/monument" + idee_monument.id + "/image" + id + ".jpg");
+}
+
+
+generateCarousel = (bn) => {
+
+  let res1 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(1)}
+                                alt="Image1"/></Carousel.Item>;
+
+  let res2 = null;
+  let res3 = null;
+  let res4 = null;
+  let res5 = null;
+  let res6 = null;
+
+  if (bn >= 2) {
+     res2 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(2)}
+                                alt="Image2"/></Carousel.Item>;
+  }
+  if (bn >= 3)
+     res3 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(3)}
+                                alt="Image3"/></Carousel.Item>;
+  if (bn >= 4)
+     res4 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(4)}
+                                alt="Image4"/></Carousel.Item>;
+  if (bn >= 5)
+     res5 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(5)}
+                                alt="Image5"/></Carousel.Item>;
+  if (bn >= 6)
+     res6 = <Carousel.Item><img className="d-block w-100"
+                                src={this.getUrl(6)}
+                                alt="Image6"/></Carousel.Item>;
+
+
+
+
+
+  return (<Carousel className="slider" slide="false">
+     {res1}
+     {res2}
+     {res3}
+     {res4}
+     {res5}
+     {res6}
+  </Carousel>);
 }
 
 
@@ -71,38 +116,7 @@ getUrl = (id) => {
 
                 <Container>
                     <Row>
-                        <Carousel className="slider" slide="false">
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(1)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(2)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(3)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(4)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(5)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        <Carousel.Item><img className="d-block w-100"
-                                                   src={this.getUrl(6)}
-                                                   alt="Image1"/></Carousel.Item>;
-
-                        <Carousel.Item/>
-                        </Carousel>
+                        {this.generateCarousel(x.nbimages)}
                     </Row>
                     <Row>
                         <h1>{x.nom_monu}</h1>
@@ -119,9 +133,9 @@ getUrl = (id) => {
                         </p>
                     </Row>
                     <Row className="ret_row">
-                        <Button className="btn_ret" variant="danger" size="lg" href="/">
+                      <Link to="/Home">  <Button className="btn_ret" variant="danger" size="lg" >
                             Retour à la carte
-                        </Button>
+                        </Button></Link>
                     </Row>
                     </div>
                 </Container>
--- src/Routes.js
+++ src/Routes.js
@@ -13,12 +13,9 @@ export default class Routes extends Component {
         return (
                 <Switch>
                      <Route path="/Menu"  component={Menu} />
-<<<<<<< HEAD
-                     <Route path="/Descriptif"  component={Descriptif} />
-=======
+
                     <Route path="/Descriptif"  component={Descriptif} />
                     <Route path="/Incontournables"  component={Incontournables} />
->>>>>>> 44f3c22b4ecb6ce4c343b2887fa929acbf737ac3
                     <Route path="/Home" component={Home} />
                     <Route path="/" component={Accueil} />
                 </Switch>
